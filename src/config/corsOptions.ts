3b80cd6c6325643a495627af263f8de6e70dacf9
Ayomide Akintan <emzyakints2005@gmail.com>
2025-07-07 11:51:12 +0100

    removed comment from corsOptions

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -11,7 +11,7 @@ export const corsOptions: cors.CorsOptions = {
     origin: string | undefined,
     callback: (error: Error | null, allow?: boolean) => void
   ) => {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+     
     if (allowedOrigins.indexOf(origin!) !== -1 || !origin) {
       callback(null, true);
     } else {
