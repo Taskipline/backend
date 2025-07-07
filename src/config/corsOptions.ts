cd7ac96929b36100b79d97c322551389e49816d7
Ayomide Akintan <emzyakints2005@gmail.com>
2025-07-07 11:54:28 +0100

    added links for corsOptions

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -4,6 +4,9 @@ const allowedOrigins: string[] = [
   "http://localhost:3001/",
   "http://localhost:3002/",
   "http://localhost:3003/",
+  "https://taskipline.netlify.app/",
+  // "https://taskipline.com/",
+  "https://taskipline.emmy-akintz.tech/",
 ];
 
 export const corsOptions: cors.CorsOptions = {
@@ -11,7 +14,6 @@ export const corsOptions: cors.CorsOptions = {
     origin: string | undefined,
     callback: (error: Error | null, allow?: boolean) => void
   ) => {
-     
     if (allowedOrigins.indexOf(origin!) !== -1 || !origin) {
       callback(null, true);
     } else {
